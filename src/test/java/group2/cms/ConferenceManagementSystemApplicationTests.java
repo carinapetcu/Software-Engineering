dacb56bbbb97d2a9b1a3a8acbb62f75141e74224
parvanescu <parvanescu.stefan@yahoo.com>
2021-04-21 15:44:46 +0300

    Initial commit

---
+++ src/test/java/group2/cms/ConferenceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package group2.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
