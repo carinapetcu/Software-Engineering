c4aea3bd62fdc215550a866891c3843aed7c1a89
Marin-Peptenaru <marin.peptenaru@protonmail.com>
2021-05-04 15:31:39 +0300

    Refactored Author Controller to return ResponseEntity<>

--- src/main/java/group2/cms/domain/PCMember.java
+++ src/main/java/group2/cms/domain/PCMember.java
@@ -13,4 +13,20 @@ import javax.persistence.Entity;
 public class PCMember extends Author{
     private String affiliation;
     private String webPage;
+
+    public PCMember(){}
+
+    public PCMember(CMSUser user, String affiliation, String webpage){
+        super(user);
+        this.authority = Authority.PCMember;
+        this.affiliation = affiliation;
+        this.webPage = webpage;
+    }
+
+    public PCMember(CMSUser user, String affiliation){
+        super(user);
+        this.authority = Authority.PCMember;
+        this.affiliation = affiliation;
+    }
+
 }
---
+++ src/main/java/group2/cms/exceptions/BackendException.java
@@ -0,0 +1,5 @@
+package group2.cms.exceptions;
+
+public class BackendException extends RuntimeException{
+    public BackendException (String msg) {super(msg);}
+}
