8fa219b9f34468f3c81546c6bf3b59d5838e4c98
carinapetcu <petcu.carina07@gmail.com>
2021-05-05 01:05:20 +0300

    Added CRUD for Chair

--- src/main/java/group2/cms/domain/Chair.java
+++ src/main/java/group2/cms/domain/Chair.java
@@ -8,4 +8,15 @@ import lombok.*;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class Chair extends PCMember{
+    public Chair(){}
+
+    public Chair(CMSUser user, String affiliation, String webpage){
+        super(user, affiliation, webpage);
+        this.authority = Authority.Chair;
+    }
+
+    public Chair(CMSUser user, String affiliation){
+        super(user, affiliation);
+        this.authority = Authority.Chair;
+    }
 }
