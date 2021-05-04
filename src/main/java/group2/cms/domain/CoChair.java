89c2004dae97566502cb3f572e53140aeb382c62
carinapetcu <petcu.carina07@gmail.com>
2021-05-05 01:22:27 +0300

    Added CRUD for CoChair

--- src/main/java/group2/cms/domain/CoChair.java
+++ src/main/java/group2/cms/domain/CoChair.java
@@ -8,4 +8,16 @@ import lombok.*;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class CoChair extends PCMember{
+    public CoChair(){
+    }
+
+    public CoChair(CMSUser user, String affiliation, String webpage){
+        super(user, affiliation, webpage);
+        this.authority = Authority.CoChair;
+    }
+
+    public CoChair(CMSUser user, String affiliation){
+        super(user, affiliation);
+        this.authority = Authority.CoChair;
+    }
 }
