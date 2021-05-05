49a460ef3d73ec99ce7e5659235d51f83714774f
carinapetcu <petcu.carina07@gmail.com>
2021-05-05 11:56:26 +0300

    Fixed minor issue regarding Serialization

--- src/main/java/group2/cms/controller/Request/ChairRequest.java
+++ src/main/java/group2/cms/controller/Request/ChairRequest.java
@@ -4,10 +4,12 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
+import java.io.Serializable;
+
 @Data
 @EqualsAndHashCode
 @ToString
-public class ChairRequest {
+public class ChairRequest implements Serializable {
     private Long userID;
     private String affiliation;
     private String website;
