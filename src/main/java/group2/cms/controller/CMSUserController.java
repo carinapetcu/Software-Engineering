27c06b990aff90c8c9d3a24d6a9ecf7357a90186
Marin-Peptenaru <marin.peptenaru@protonmail.com>
2021-05-05 16:14:16 +0300

    Implemented Service and Controller for CMSUser. Please check if it is ok on your computer as on mine IntelliJ can't find CMSUserService and import it in the Controller class.

---
+++ src/main/java/group2/cms/controller/CMSUserController.java
@@ -0,0 +1,105 @@
+package group2.cms.controller;
+
+import group2.cms.controller.Request.CMSUserRequest;
+import group2.cms.exceptions.BackendException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import group2.cms.service.CMSUserService;
+
+import java.util.Optional;
+
+@RestController
+public class CMSUserController {
+
+    @Autowired
+    private CMSUserService users;
+
+    @PostMapping("users/add")
+    public ResponseEntity<?> addUser(@RequestBody CMSUserRequest request) {
+        try {
+            return new ResponseEntity<>(
+                    users.addUser(request.getFullName(), request.getEmail(),
+                            request.getUsername(), request.getPassword()),
+                    HttpStatus.OK
+            );
+        } catch (BackendException e) {
+            return new ResponseEntity<>(
+                    e.getMessage(),
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+    }
+
+    @PostMapping("users/update")
+    public ResponseEntity<?> updateUser(@RequestBody CMSUserRequest request) {
+        try {
+            var fullName = request.getFullName();
+            var email = request.getEmail();
+            var username = request.getUsername();
+            var password = request.getPassword();
+
+            return new ResponseEntity<>(
+                    users.updateUser(
+                            fullName.isBlank() ? Optional.empty() : Optional.of(fullName),
+                            email.isBlank() ? Optional.empty() : Optional.of(email),
+                            username.isBlank()? Optional.empty() : Optional.of(username),
+                            password.isBlank()? Optional.empty() : Optional.of(password)),
+                    HttpStatus.OK
+            );
+        } catch (BackendException e) {
+            return new ResponseEntity<>(
+                    e.getMessage(),
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+    }
+
+    @DeleteMapping("users/delete")
+    public ResponseEntity<?> deleteUser(@RequestBody CMSUserRequest request){
+        try{
+            var userID = request.getId();
+            users.deleteUser(userID);
+            return new ResponseEntity<>(
+                    "User with ID: " + userID + "deleted.",
+                    HttpStatus.OK
+            );
+        }catch(BackendException e){
+            return new ResponseEntity<>(
+                    e.getMessage(),
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+    }
+
+    @GetMapping("users/list")
+    public ResponseEntity<?> getAllUsers(){
+        try{
+            return new ResponseEntity<>(
+                    users.getAllUsers(),
+                    HttpStatus.OK
+            );
+        }catch (BackendException e){
+            return new ResponseEntity<>(
+                    e.getMessage(),
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+    }
+
+    @GetMapping("users/list/{userID}")
+    public ResponseEntity<?> getUserByID(@PathVariable Long userID){
+        try{
+            return new ResponseEntity<>(
+                    users.getUserByID(userID),
+                    HttpStatus.OK
+            );
+        }catch (BackendException e){
+            return new ResponseEntity<>(
+                    e.getMessage(),
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+    }
+}
---
+++ src/main/java/group2/cms/service/CMSUserService.java
@@ -0,0 +1,89 @@
+package group2.cms.service;
+
+
+import group2.cms.domain.CMSUser;
+import group2.cms.exceptions.CredentialsAlreadyInUseException;
+import group2.cms.exceptions.InvalidCredentialsException;
+import group2.cms.exceptions.InvalidIDException;
+import group2.cms.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CMSUserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private void throwExceptionIfCredentialsAlreadyInUse(String username, String email) {
+        var errorMessageBuilder = new StringBuilder();
+        errorMessageBuilder.append(
+                userRepository.existsByUsername(username) ? "Username" + username + "already in use.\n" : ""
+        ).append(
+                userRepository.existsByEmail(email) ? "Email address" + email + "already in use.\n" : ""
+        );
+
+        if (!errorMessageBuilder.isEmpty())
+            throw new CredentialsAlreadyInUseException(errorMessageBuilder.toString());
+
+    }
+
+
+    public CMSUser addUser(String fullName,  String email, String username, String password) {
+        throwExceptionIfCredentialsAlreadyInUse(username, email);
+        return userRepository.save(new CMSUser(
+                fullName,
+                email,
+                username,
+                password
+        ));
+    }
+
+    public CMSUser updateUser(Long userID, Optional<String> fullName, Optional<String> email, Optional<String> username,
+                              Optional<String> password){
+        var user = userRepository.findById(userID).orElseThrow(
+                () -> new InvalidIDException("Invalid User ID: " + userID)
+        );
+
+        throwExceptionIfCredentialsAlreadyInUse(
+                username.orElse(""),
+                email.orElse(""));
+
+        fullName.ifPresent(user::setFullName);
+        username.ifPresent(user::setUsername);
+        email.ifPresent(user::setEmail);
+        password.ifPresent(user::setPassword);
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long userID){
+        userRepository.deleteById(userID);
+    }
+
+    public CMSUser getUserByID(Long userID){
+        return userRepository.findById(userID).orElseThrow(
+                () -> new InvalidIDException("Invalid User ID: " + userID)
+        );
+    }
+
+    public CMSUser getUserByUsername(String username){
+        return userRepository.findByUsername(username).orElseThrow(
+                () -> new InvalidCredentialsException("There is no user with the username: " + username)
+        );
+    }
+
+    public CMSUser getUserByEmail(String email){
+        return userRepository.findByEmail(email).orElseThrow(
+                () -> new InvalidCredentialsException("There is no user with the given email address: " + email)
+        );
+    }
+
+    public List<CMSUser> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+}
