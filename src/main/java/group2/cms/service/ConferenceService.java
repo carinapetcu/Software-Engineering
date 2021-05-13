22ea7e04de34f9e0d4f04dd12148a5dc0649cd25
Alex Petrean <alex.petrean@outlook.com>
2021-05-13 16:51:47 +0300

    feature: create ConferenceService class and autowire the repository

---
+++ src/main/java/group2/cms/service/ConferenceService.java
@@ -0,0 +1,10 @@
+package group2.cms.service;
+
+import group2.cms.repository.ConferenceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ConferenceService {
+    @Autowired
+    private ConferenceRepository repository;
+
+}
