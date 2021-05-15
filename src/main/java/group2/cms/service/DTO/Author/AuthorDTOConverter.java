3680c4f66b66df2317edb29f3fff94ddd7edff07
Marin-Peptenaru <marin.peptenaru@protonmail.com>
2021-05-15 19:37:45 +0300

    Finished author dto converter

--- src/main/java/group2/cms/service/DTO/Author/AuthorDTOConverter.java
+++ src/main/java/group2/cms/service/DTO/Author/AuthorDTOConverter.java
@@ -1,14 +1,24 @@
 package group2.cms.service.DTO.Author;
 
 import group2.cms.domain.Author;
+import group2.cms.domain.CMSUser;
 import group2.cms.service.DTO.DTOConverter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthorDTOConverter implements DTOConverter<Author, AuthorDTO> {
+    
+    /// Paper must be retrieved (if present in the database) and set manually with author.setPaper() by the service
     @Override
     public Author dtoToEntity(AuthorDTO dto) {
-        return new Author();
+        var user = new CMSUser(
+                dto.getFullName(),
+                dto.getEmail(),
+                dto.getUsername(),
+                dto.getPassword()
+        );
+        user.setId(dto.getUserID());
+        return new Author(user);
     }
 
     @Override
